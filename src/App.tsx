8a03b82a2f497460e9e2a779a472796520214e0b
Prashant Dahal <Prashantdahal319@gmail.com>
2025-06-06 08:22:28 +0545

    added new files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+      </Route>
+  </Routes>
+  );
+}
+
+export default App;
